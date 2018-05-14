688c74a90cb20ed47e0a8635d2d2fbee6193d467
Jed Peek <jedpeek@yahoo.com>
2018-05-14 14:46:38 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -26,9 +26,7 @@ def won?(board)
 
     if (pos_1 == "X" && pos_2 == "X" && pos_3 == "X") || (pos_1 == "O" && pos_2 == "O" && pos_3 == "O")
       return win_combo
-    else
-      return false
     end
-
+return false
   end
 end
